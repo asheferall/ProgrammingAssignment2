69c8147863f7382eb8a45a43aba032f153a0dcad
Jeremy Lai <jei@qlik.com>
2017-09-01 09:52:23 +0800

    Jeremy Lai

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Provides functions to set and get the matrix and invese of the matrix. 
+# Also caches the invese of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverseMatrix <- NULL
+	
+	set <- function(y) { 
+		x <<- y
+		inverseMatrix <<- NULL
+	}
+	
+	get <- function() x
+	
+	setInverseMatrix <- function(m) inverseMatrix <<- m
+	
+	getInverseMatrix <- function() inverseMatrix
+	
+	list(set = set,
+		 get = get,
+		 setInverseMatrix = setInverseMatrix,
+		 getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+# Checks whether the inverse has been calculated if not calculates it and stores it in the cacheSolve
+# Returns the invese matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverseMatrix()
+	
+	if (!is.null(m)) { 
+		message("Getting Cached Invese Matrix")
+		return(m)
+	}
+	
+	data <- x$get()
+	m <- solve(data)
+	x$setInverseMatrix(m)
+	
+	m
 }
